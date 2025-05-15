fd4f6a2bf1ddafa0cbf17fa6660ef60d5520f03a
Murodbek <arslonovmurodbek071@gmail.com>
2025-05-15 09:18:48 +0500

    sdfsadf

--- js/home.js
+++ js/home.js
@@ -1,18 +1,14 @@
 let budilnikBtn = document.querySelector(".budilnikBtn");
-let timerkBtn = document.querySelector(".timerkBtn");
 let kamera = document.querySelector(".kamera img");
-let kameraIcon = document.querySelector(".kamera-icon");
+let kameraIcon = document.querySelector(".kamera-icon img");
 
-kameraIcon.addEventListener("click", () => {
+kamera.addEventListener("click", () => {
   location.href = "pages/kamera.html";
 });
-kamera.addEventListener("click", () => {
+kameraIcon.addEventListener("click", () => {
   location.href = "pages/kamera.html";
 });
 
 budilnikBtn.addEventListener("click", () => {
   location.href = "pages/budulnik.html";
-});
-timerkBtn.addEventListener("click", () => {
-  location.href = "pages/timer.html";
 });
